d16663bc313d09459687b6ffd30b2588b1363a41
Mikolaj Kostrzewa <mikostrzewa1618@gmail.com>
2025-04-17 15:57:45 -0400

    Basic

---
+++ src/turtlebot3_controller/turtlebot3_controller/wall_following.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+import statistics
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+class DrivingNode(Node):
+
+    def __init__(self):
+        super().__init__('driving_node')
+        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.5  # seconds
+        self.subscriber = self.create_subscription(LaserScan,'scan',self.listener_callback,10)
+
+    def turn_left(self):
+        movement_msg = Twist()
+        movement_msg.linear.x = 0.0
+        movement_msg.linear.y = 0.0
+        movement_msg.linear.z = 0.0
+        movement_msg.angular.x = 0.0
+        movement_msg.angular.y = 0.0
+        movement_msg.angular.z = 1.0
+        return movement_msg
+    
+    def turn_right(self):
+        movement_msg = Twist()
+        movement_msg.linear.x = 0.0
+        movement_msg.linear.y = 0.0
+        movement_msg.linear.z = 0.0
+        movement_msg.angular.x = 0.0
+        movement_msg.angular.y = 0.0
+        movement_msg.angular.z = -1.0
+        return movement_msg
+    
+    def move_forward(self):
+        movement_msg = Twist()
+        movement_msg.linear.x = 0.1
+        movement_msg.linear.y = 0.0
+        movement_msg.linear.z = 0.0
+        movement_msg.angular.x = 0.0
+        movement_msg.angular.y = 0.0
+        movement_msg.angular.z = 0.0
+        return movement_msg
+    
+    def move_backward(self):
+        movement_msg = Twist()
+        movement_msg.linear.x = -0.1
+        movement_msg.linear.y = 0.0
+        movement_msg.linear.z = 0.0
+        movement_msg.angular.x = 0.0
+        movement_msg.angular.y = 0.0
+        movement_msg.angular.z = 0.0
+        return movement_msg
+    
+    def stop(self):
+        movement_msg = Twist()
+        movement_msg.linear.x = 0.0
+        movement_msg.linear.y = 0.0
+        movement_msg.linear.z = 0.0
+        movement_msg.angular.x = 0.0
+        movement_msg.angular.y = 0.0
+        movement_msg.angular.z = 0.0
+        return movement_msg
+    
+    def check_wall_in_front(self, msg, distance):
+        range = msg.ranges[175*2:185*2]
+        median = statistics.median(range)
+        if (median < distance):
+            return True
+        else:
+            return False
+    
+    def check_wall_on_left(self, msg, distance):
+        range = msg.ranges[85*2:95*2]
+        median = statistics.median(range)
+        if (median < distance):
+            return True
+        else:
+            return False
+    def check_wall_on_right(self, msg, distance):
+        range = msg.ranges[265*2:275*2]
+        median = statistics.median(range)
+        if (median < distance):
+            return True
+        else:
+            return False
+    def check_wall_on_back(self, msg, distance):
+        range = msg.ranges[355*2:365*2]
+        median = statistics.median(range)
+        if (median < distance):
+            return True
+        else:
+            return False
+
+    
+        
+    def listener_callback(self,msg):
+        
+        range = msg.ranges[350:370]
+        median = statistics.median(range)
+        movement_msg = Twist()
+        if (median > 1000 or median < 0.4):
+            print("NOT DRIVING")
+            print(median)
+            movement_msg = self.stop
+        else:
+            print("DRIVING")
+            movement_msg = self.move_forward()
+        self.publisher.publish(movement_msg)
+
+    
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    driving_node = DrivingNode()
+
+    rclpy.spin(driving_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    driving_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
