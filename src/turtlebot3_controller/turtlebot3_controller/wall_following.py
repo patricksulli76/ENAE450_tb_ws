fa3eec943222cd8a51f9b5dac16600f5f298683e
Mikolaj Kostrzewa <mikostrzewa1618@gmail.com>
2025-04-17 17:09:54 -0400

    Newest

--- src/turtlebot3_controller/turtlebot3_controller/wall_following.py
+++ src/turtlebot3_controller/turtlebot3_controller/wall_following.py
@@ -17,7 +17,7 @@ class DrivingNode(Node):
         self.turning_right = False
         self.turning_left = False
         self.speed = 0.1
-        self.distance = 0.4
+        self.distance = 0.5
         self.turn_speed = 1.0
         self.crit_distance = 0.3
 
@@ -105,8 +105,8 @@ class DrivingNode(Node):
     
     def check_wall_all(self, msg, distance):
         range = msg.ranges[0:720]
-        median = statistics.median(range)
-        if (median < distance):
+        min = min(range)
+        if (min < distance):
             return True
         else:
             return False
@@ -148,17 +148,18 @@ class DrivingNode(Node):
         movement_msg = Twist()
         movement_msg = self.stop()
         if self.following_right:
-            if(not self.check_wall_on_right(msg, self.distance) or self.check_wall_all(msg, self.crit_distance)):
-                print("TURNING LEFT")
-                movement_msg = self.turn_left()
-                self.following_right = True
+            print("Following right")
+            if(self.check_wall_all(msg, self.crit_distance)):
+                self.movement_msg = self.move_backward()
+                self.movement_msg.angular.z = self.turn_speed
             elif (self.check_wall_on_right and not self.check_wall_in_front(msg, self.distance)):
                 movement_msg = self.move_forward()
                 print("DRIVING")
             else:
                 print("TURNING Left")
-                movement_msg = self.turn_left()
+                movement_msg = self.turn_right()
         elif self.following_left:
+            print("Following left")
             if(self.check_wall_in_front(msg, self.distance)):
                 print("TURNING RIGHT")
                 movement_msg = self.turn_right()
@@ -175,14 +176,16 @@ class DrivingNode(Node):
                 print("DRIVING")
                 print("Wall: %f", self.check_wall_in_front(msg, self.distance))
                 movement_msg = self.move_forward()
-            elif (not self.check_wall_on_left(msg, self.distance)):
-                print("TURNING RIGHT")
-                movement_msg = self.turn_right()
-                self.following_right = True
+            
             elif (not self.check_wall_on_right(msg, self.distance)):
                 print("TURNING LEFT")
                 movement_msg = self.turn_left()
-                self.following_left = True
+            elif (not self.check_wall_on_left(msg, self.distance)):
+                print("TURNING RIGHT")
+                movement_msg = self.turn_right()
+
+            if (self.check_wall_on_right(msg, self.distance)):
+                self.following_right = True
         self.publisher.publish(movement_msg)
 
 
