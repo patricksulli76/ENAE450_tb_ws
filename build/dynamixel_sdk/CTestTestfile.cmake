b89f24ca01dcfe74bf1bbf03dd98b2829136d28e
patricksulli76 <113055515+patricksulli76@users.noreply.github.com>
2025-05-01 16:49:16 -0400

    adding

--- build/dynamixel_sdk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/patrick/ENAE450_tb_ws/src/DynamixelSDK/ros/dynamixel_sdk
-# Build directory: /home/patrick/ENAE450_tb_ws/build/dynamixel_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlebot3_fake_node/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/patrick/ENAE450_tb_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
